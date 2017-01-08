3a5c0e82f341c4fecab5a41e3b61b067a01059b7
Aaron Endsley <aaronendsley@gmail.com>
2017-01-08 15:29:22 -0600

    Created a function that allows the user to create a score object with the starting score, the passing score and the failing score

--- js/Quiz_creator.js
+++ js/Quiz_creator.js
@@ -1,12 +1,10 @@
 // What this library will do 
-
 // 1.   get the name of the test and and have the name of the test be the container for the main test data
 // 2.   Create an object that contains the question, answer, and the distractors as an array and push to the main test container
 // 2.1  Be able to allow the user to create single Answer questions and multiple answer questions
 // 3    Create a function that checks the answer and returns true 
 // 4    Create the ability for the user to define a score for the test on the test object
 // 5    Create a function that will allow the user to define what is a passing score and what is not a passing score
-
 var qc = (function quizCreator(){
     function createTest(nameOfTest){
         var test = {
@@ -17,46 +15,55 @@ var qc = (function quizCreator(){
 
         return test;
     }
+    
+    function createScoreRequirements(test ,passingScore, failingScore){
+        if(typeof passingScore === 'number' && typeof failingScore === 'number'){
+        test.score = {};
+        test.score.currentScore = 0;
+        test.score.passingScore = passingScore;
+        test.score.failingScore = failingScore;
+        }else{
+            console.log(`${passingScore} and ${failingScore} need to be a number`);
+        }
+    }
     function createSingleAnwerQuestion(question, answer, distractors ){
     var questionToReturn = {
         question:question,
         answer:answer,
         distractors:distractors
     };
-
     if(Array.isArray(distractors)){
     return questionToReturn;
     }else{
         console.log(`${distractors} is not an array, please enter your distractors as an array, your question is currently set to undefined as a result`);
     }
     }
-
     function multipleSelectQuestion(question, answers, distractors){
          var questionToReturn ={
              question:question,
              answers:answers,
              distractors:distractors
          };   
-
     if(Array.isArray(answers) && Array.isArray(distractors)){
         return questionToReturn;    
     }else{
         console.log(`${answers} or ${distractors} must be entered as arrays, your question is currently set to undefined`);
     } 
-
     }
-
     return{
         createTest:createTest,
+        createScoreRequirements:createScoreRequirements,
         createSingleAnwerQuestion:createSingleAnwerQuestion,
         multipleSelectQuestion:multipleSelectQuestion
     };
 })();
 
-var test = qc.createTest('bob'); 
+var test = qc.createTest('bob');
+qc.createScoreRequirements(test.bob, 70, 69); 
 test.bob.question1 = qc.createSingleAnwerQuestion('Why do birds suddenly appear', 'because you are near', ['Sandwhiches', 'Psycotic Space Nuns', 'Sasquatch']);
 test.bob.question2= qc.createSingleAnwerQuestion('What color is the sky', 'blue', ['bill', 'Jim Raynors Smile', 'Zerg Creep']);
 test.bob.question3 = qc.multipleSelectQuestion('What does Captain Picard Like?', ['Tea Earl Grey Hot', 'Riding Horses'], ['Children', 'Lots of Noise']);
-test.bob.question4 = qc.multipleSelectQuestion('What does the fox say', 'ning ning ning', ['I would like some tea', 'nope']);
-test.bob.question5 = qc.multipleSelectQuestion('who is the flash', 'Barry Allen', 'Sindregosa');
-console.log(test);
+//test.bob.question4 = qc.multipleSelectQuestion('What does the fox say', 'ning ning ning', ['I would like some tea', 'nope']);
+//test.bob.question5 = qc.multipleSelectQuestion('who is the flash', 'Barry Allen', 'Sindregosa');
+console.log(test.bob);
+
