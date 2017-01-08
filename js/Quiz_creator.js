bd162d0fe8fcd1b1a4e8d42a9f539d0d05a36b97
Aaron Endsley <aaronendsley@gmail.com>
2017-01-08 14:19:22 -0600

    added a check to make sure that distractors are an array

--- js/Quiz_creator.js
+++ js/Quiz_creator.js
@@ -6,7 +6,7 @@
 // 3    Create a function that checks the answer and returns true 
 // 4    Create the ability for the user to define a score for the test on the test object
 // 5    Create a function that will allow the user to define what is a passing score and what is not a passing score
- 
+
 var qc = (function quizCreator(){
     function createTest(nameOfTest){
         var test = {
@@ -23,7 +23,13 @@ var qc = (function quizCreator(){
         answer:answer,
         distractors:distractors
     }
+
+    if(Array.isArray(distractors)){
+        console.log(`${distractors} is indeed an array`);
     return questionToReturn;
+    }else{
+        console.log(`${distractors} is not an array, please enter your distractors as an array, your question is currently set to undefined as a result`);
+    }
     }
 
     return{
@@ -34,4 +40,5 @@ var qc = (function quizCreator(){
 
 var test = qc.createTest('bob'); 
 test.bob.question1 = qc.createSingleAnwerQuestion('Why do birds suddenly appear', 'because you are near', ['Sandwhiches', 'Psycotic Space Nuns', 'Sasquatch']);
+test.bob.question2= qc.createSingleAnwerQuestion('What color is the sky', 'blue', ['bill', 'Jim Raynors Smile', 'Zerg Creep']);
 console.log(test);
