350459752273bc70d63ff2715fd2188ac04aedc4
Aaron Endsley <aaronendsley@gmail.com>
2017-01-08 14:05:50 -0600

    added objectives to the project

--- js/Quiz_creator.js
+++ js/Quiz_creator.js
@@ -1,3 +1,12 @@
+// What this library will do 
+
+// 1.   get the name of the test and and have the name of the test be the container for the main test data
+// 2.   Create an object that contains the question, answer, and the distractors as an array and push to the main test container
+// 2.1  Be able to allow the user to create single Answer questions and multiple answer questions
+// 3    Create a function that checks the answer and returns true 
+// 4    Create the ability for the user to define a score for the test on the test object
+// 5    Create a function that will allow the user to define what is a passing score and what is not a passing score
+ 
 var qc = (function quizCreator(){
     function createTest(nameOfTest){
         var test = {
