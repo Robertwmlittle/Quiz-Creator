1c69995d7300af6ddc07c16f92b34b532a3bc7b1
Aaron Endsley <aaronendsley@gmail.com>
2017-01-13 22:46:27 -0600

    update

--- js/Quiz_creator.js
+++ js/Quiz_creator.js
@@ -18,16 +18,22 @@ var qc = (function quizCreator() {
     }
 
     function createSingleAnwerQuestion(question, answers, distractors, questionWorth) {
-        var questionDistractor = distractors; 
+        var questionDistractor = distractors;
         var questionToReturn = {
             question: question,
-            answers: {answer:answers, isClicked:false},
+            answers: {
+                answer: answers,
+                isClicked: false
+            },
             Distractor: [],
             questionWorth: questionWorth,
             isCorrect: false
         };
-         for(let i= 0; i < questionDistractor.length; i++ ){
-                questionToReturn.Distractor.push({distractor:questionDistractor[i], isClicked:false});
+        for (let i = 0; i < questionDistractor.length; i++) {
+            questionToReturn.Distractor.push({
+                distractor: questionDistractor[i],
+                isClicked: false
+            });
         }
 
         if (typeof questionWorth === 'number') {
@@ -42,21 +48,27 @@ var qc = (function quizCreator() {
     }
 
     function multipleSelectQuestion(question, answers, distractors, questionWorth) {
-         var questionAnswers = answers;
+        var questionAnswers = answers;
         var questionDistractors = distractors;
         var questionToReturn = {
             question: question,
             Answers: [],
-            Distractors:[],
+            Distractors: [],
             questionWorth: questionWorth,
             isCorrect: false
         };
-        for(let i = 0; i < questionAnswers.length; i++){
-                questionToReturn.Answers.push({answer:questionAnswers[i], isClicked:false}); 
+        for (let i = 0; i < questionAnswers.length; i++) {
+            questionToReturn.Answers.push({
+                answer: questionAnswers[i],
+                isClicked: false
+            });
         }
 
-        for(let i= 0; i < questionDistractors.length; i++ ){
-                questionToReturn.Distractors.push({distractor:questionDistractors[i], isClicked:false});
+        for (let i = 0; i < questionDistractors.length; i++) {
+            questionToReturn.Distractors.push({
+                distractor: questionDistractors[i],
+                isClicked: false
+            });
         }
         if (typeof questionWorth === 'number') {
             if (Array.isArray(answers) && Array.isArray(distractors)) {
@@ -97,7 +109,7 @@ qc.createScoreRequirements(test.bob, 70, 69);
 test.bob.question1 = qc.createSingleAnwerQuestion('Why do birds suddenly appear', 'because you are near', ['Sandwhiches', 'Psycotic Space Nuns', 'Sasquatch'], 10);
 //test.bob.question2 = qc.createSingleAnwerQuestion('What color is the sky', 'blue', ['bill', 'Jim Raynors Smile', 'Zerg Creep'], 'two');
 //test.bob.question3 = qc.multipleSelectQuestion('What does Captain Picard Like?', ['Tea Earl Grey Hot', 'Riding Horses'], ['Children', 'Lots of Noise'], 23);
-test.bob.question2= qc.multipleSelectQuestion('what does the fox say', ['jim', 'phil'], ['howard', 'steven', 'devin'], 20);
+test.bob.question2 = qc.multipleSelectQuestion('what does the fox say', ['jim', 'phil'], ['howard', 'steven', 'devin'], 20);
 //test.bob.question2.isCorrect = true;
 qc.changeCorrectState(test.bob.question3);
 //qc.changeCorrectState(test.bob.question2);
